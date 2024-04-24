ea3886a31422bbc65f5ae79bb51c37c273e345c7
Jon <jonnysoco@gmail.com>
2024-04-23 21:58:48 -0500

    commit 423958

--- src/components/shared/GrowLine/GrowLine.tsx
+++ src/components/shared/GrowLine/GrowLine.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './_GrowLine.scss';
-import AOS from 'aos';
-// import 'aos/dist/aos.css';
 
-// // Initialize AOS
-// AOS.init();
+
 
 interface GrowLineProps {
   className?: string;
 }
 const GrowLine: React.FC<GrowLineProps> = ({ className = '' }) => {
-  // Combine the grow-div class with any additional classes passed via props
   const combinedClassName = `grow-div ${className || ''}`.trim();
 
   return (
